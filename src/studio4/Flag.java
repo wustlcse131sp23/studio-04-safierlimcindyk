4e155e56b738c3cf87da966fa1d7812622024b03
micha <micha@Franklin.dhcp.wustl.edu>
2023-02-23 12:47:47 -0600

    VICKY FLAG

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(0.5, 0.5, 0.3, 0.4);
+		
+		StdDraw.setPenColor(Color.CYAN);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
+		
+		Color purple = new Color (255, 0, 255);
+		StdDraw.setPenColor(purple);
+		
+		double[] xCoord = new double[] {0.3, 0.5, 0.7};
+		double[] yCoord = new double[] {0.3, 0.5, 0.3};
+		StdDraw.filledPolygon(xCoord, yCoord);
+		
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.text(0.5, 0.2, "VICKY");
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,28 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		StdDraw.setPenColor(red, green, blue);
+		if (shape.equals("triangle")) {
+			double [] xCoordinates = new double [3];
+			double [] yCoordinates = new double [3];
+			for (int d = 0; d < 3; d++) {
+				xCoordinates[d] = in.nextDouble();
+				yCoordinates[d] = in.nextDouble();
+			}
+			StdDraw.filledPolygon(xCoordinates, yCoordinates);
+		}
+		else {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+		StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		}
 		
 	}
 }
